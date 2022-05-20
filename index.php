557d21cfb9044566bcacba2b33f866111c30fafa
Sonia Chalal <contact@soniawebdesigner.fr>
2022-05-20 23:20:20 +0200

    exercice travail-01 terminé

--- index.php
+++ index.php
@@ -5,7 +5,8 @@
      <li><a href="./03-tableaux/index.php">03-tableaux</a></li>
      <li><a href="./04-conditions/index.php">04-conditions</a></li>
      <li><a href="./05-boucles/index.php">05-boucles</a></li>
-     <li><a href="./travaux/travail-00.php/index.php">travaux</a></li>
+     <li><a href="./travaux/travail-00.php/index.php">travail-00</a></li>
+     <li><a href="./travaux/travail-01.php/index.php">travail-01</a></li>
      <ul>
           <li>
               <a href="https://www.php.net/manual/fr/book.array.php">Document PHP array</a>
--- travaux/travail-01.php
+++ travaux/travail-01.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 <?php
 /*
 Travail-01 :
@@ -13,4 +22,33 @@ Travail-01 :
 | 35 | 95 |
 | 11 | 51.8 |
  */
+?>
+<table class="table table-striped table-responsive">
+    <tr>
+        <td>| °C | °F |</td>
+    </tr>
+    <tr>
+        <td>| --- | --- |</td>
+    </tr>
+    <?php
+        $celsius = [25,03,35,11];
+        foreach ($celsius as $key => $value)
+        {
+        //Pour chaque valeur du tableau celsius je converti en Fahrenheit
+        $fahrenheit = ($value * 9/5) + 32;
+    ?>
+    <tbody>
+    <tr>
+        <td>
+            <?php echo '| ' . str_pad($value, 2, '0', STR_PAD_LEFT) .' |'; ?>
+            <?php echo round($fahrenheit,1,PHP_ROUND_HALF_UP) .' | '; ?>
+        </td>
+    </tr>
+        <?php } ?>
+   </tbody>
+</table>
+</body>
+</html>
+
+
 
